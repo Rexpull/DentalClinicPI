5672ebeaa7310e547ff7f9a723476da2faa6ebcc
Pedr1Hsp <PHenrique789052@gmail.com>
2023-11-07 23:46:26 -0300

    Comit do side
    Junta essa porra corno

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import 'devextreme/dist/css/dx.light.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,8 +9,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <App />
-   <ChakraProvider/>
-   
+    
   </React.StrictMode>
 );
 
